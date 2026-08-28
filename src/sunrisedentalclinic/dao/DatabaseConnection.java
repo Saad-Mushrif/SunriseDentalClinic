a656449964c9643c1792273cd7973b040c487843
Saad-Mushrif <saadmushrif7@gmail.com>
2026-08-28 22:06:00 +0530

    Database connection.

--- src/sunrisedentalclinic/dao/DatabaseConnection.java
+++ src/sunrisedentalclinic/dao/DatabaseConnection.java
@@ -1,13 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package sunrisedentalclinic.dao;
 
-/**
- *
- * @author saadm
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DatabaseConnection {
-    
+    public static Connection getConnection(){
+        Connection con = null;
+        try {
+            Class.forName(
+            "com.mysql.jdbc.Driver");
+
+            con = DriverManager.getConnection(
+            "jdbc:mysql://localhost/sunrisedental_db",
+            "root",
+            "");
+            System.out.println("Database Connected");
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        return con;
+    }
 }
